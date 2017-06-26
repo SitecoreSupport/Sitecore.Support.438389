f19e724c913b69b703e395fb21bcfd11123b731a
Dmitry Baranov <dbr@sitecore.net>
2017-06-26 12:19:48 +0300

    no message

---
+++ src/Sitecore.Support.438389/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.438389")]
+[assembly: AssemblyProduct("Sitecore.Support.438389")]
+[assembly: ComVisible(false)]
